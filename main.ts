457b4a1ca63825eec506e1e807ee69570d00e91c
TheRealSigmaADK <aidandk5130@gmail.com>
2025-04-09 22:15:18 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    sprites.destroy(mySprite2, effects.disintegrate, 5000)
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite3.sayText("Don’t listen to that dumb flower", 5000, false)
     animation.runImageAnimation(
     mySprite2,
     [img`
@@ -75,7 +79,13 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     )
     mySprite2.sayText("Hi I’m Flowe- you probably already know this ", 5000, false)
 })
+sprites.onDestroyed(SpriteKind.Enemy, function (sprite) {
+    mySprite3.sayText("Told ya", 5000, false)
+    mySprite.sayText("Explore the demo", 5000, false)
+})
+let mySprite3: Sprite = null
 let mySprite2: Sprite = null
+let mySprite: Sprite = null
 scene.setBackgroundImage(img`
     ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
     ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
@@ -198,15 +208,15 @@ scene.setBackgroundImage(img`
     bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
     bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
     `)
-let mySprite = sprites.create(img`
+mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . e e e e e e e e . . . 
     . . . . . e e e e e e e e . . . 
-    . . . . . e e 5 e 5 e e e . . . 
-    . . . . . e f f 5 f f e 5 . . . 
+    . . . . . e e f e f e e e . . . 
+    . . . . . e f f f f f e 5 . . . 
     . . . . . e 5 5 5 5 5 5 e . . . 
     . . . . . e 5 5 f f 5 5 e . . . 
     . . . . . e e e 5 5 e e e . . . 
@@ -216,7 +226,6 @@ let mySprite = sprites.create(img`
     . . . . 5 a f a a a a f a 5 . . 
     . . . . a 5 f 5 5 5 5 f 5 a . . 
     `, SpriteKind.Player)
-controller.moveSprite(mySprite)
 music.play(music.createSong(assets.song`Worthless`), music.PlaybackMode.LoopingInBackground)
 tiles.setCurrentTilemap(tilemap`level1`)
 mySprite2 = sprites.create(img`
@@ -236,5 +245,25 @@ mySprite2 = sprites.create(img`
     . . . . . . . . . . . 7 7 7 . . 
     . . . . . . . . . . . . 7 7 . . 
     . . . . . . . . . . . . 7 7 . . 
-    `, SpriteKind.Player)
+    `, SpriteKind.Enemy)
 mySprite2.setPosition(134, 49)
+mySprite3 = sprites.create(img`
+    . . . . . 1 1 1 1 1 9 9 9 9 9 . 
+    . . . . 1 f f f 1 9 9 9 9 9 9 . 
+    . . . . 1 f 1 f 1 9 9 9 9 9 . . 
+    . . . . 1 f f f 1 9 9 9 . . . . 
+    . . . . 1 1 1 1 1 1 f 1 . . . . 
+    . . . . . 1 f f f f 1 . . . . . 
+    . . . . 1 1 1 1 1 1 1 1 . . . . 
+    . 8 8 8 f 8 b 8 b 8 f 8 8 8 . . 
+    . 8 8 8 f 8 b 8 b 8 f 8 8 8 . . 
+    . 8 8 8 f 8 1 8 1 8 f 8 8 8 . . 
+    . 8 8 8 f 8 8 8 8 8 f 8 8 8 . . 
+    . 8 8 8 f 8 8 8 8 8 f 8 8 8 . . 
+    . 8 8 8 f 8 8 8 8 8 f 8 8 8 . . 
+    . 8 8 8 f 8 8 8 8 8 f 8 8 8 . . 
+    . 8 8 8 f 8 8 8 8 8 f 8 8 8 . . 
+    . 8 8 8 f 8 8 8 8 8 f 8 8 8 . . 
+    `, SpriteKind.Player)
+mySprite3.setPosition(138, 54)
+mySprite.setPosition(140, 76)
